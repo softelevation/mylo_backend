46fb87341cc713dd501cdf69f38aaaab002d3467
vijay sandhu <vijay.pro2020@gmail.com>
2021-03-01 17:20:38 +0530

    fix

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -61,7 +61,13 @@ async function customer_reqest(req, res, next){
 	const qb = await dbs.get_connection();
 	try {
 		const user = await jwt.verify(req.headers.authorization, accessTokenSecret);
-		const upcoming = await qb.select(['users.name','users.email','users.phone_no','users.image','users.address','users.qualifications','book_nows.status','users.about_me','book_nows.id','book_nows.created_at','book_nows.updated_at']).where('book_nows.status','pending').or_where('book_nows.status', 'in_progress').from('book_nows').join('users','users.id=book_nows.cus_id').order_by('book_nows.id','desc').get();
+		let users = await qb.select('status').where('id',user.id).limit(1).get('users');
+		let upcoming = {};
+		if(users[0].status == '1'){
+			upcoming = await qb.select(['users.name','users.email','users.phone_no','users.image','users.address','users.qualifications','book_nows.status','users.about_me','book_nows.id','book_nows.created_at','book_nows.updated_at']).where('book_nows.status','pending').or_where('book_nows.status', 'in_progress').from('book_nows').join('users','users.id=book_nows.cus_id').order_by('book_nows.id','desc').get();
+		}else{
+			upcoming = await qb.select(['users.name','users.email','users.phone_no','users.image','users.address','users.qualifications','book_nows.status','users.about_me','book_nows.id','book_nows.created_at','book_nows.updated_at']).where('book_nows.status', 'in_progress').from('book_nows').join('users','users.id=book_nows.cus_id').order_by('book_nows.id','desc').get();
+		}
 		const completed = await qb.select(['users.name','users.email','users.phone_no','users.image','users.address','users.qualifications','book_nows.status','users.about_me','book_nows.id','book_nows.created_at','book_nows.updated_at']).where('book_nows.status','completed').or_where('book_nows.status', 'rejected').or_where('book_nows.status', 'cancelled').from('book_nows').join('users','users.id=book_nows.cus_id').order_by('book_nows.id','desc').get();
 		return res.json(halper.api_response(1,'Customer request',{upcoming:upcoming,completed:completed}));
 	} catch (err) {

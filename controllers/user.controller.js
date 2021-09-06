b6da46ef20ffb3e63363b47d91ddc18eeb7718cf
vijay sandhu <vijay.pro2020@gmail.com>
2021-09-06 16:31:46 +0530

    fix code

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -237,7 +237,7 @@ async function customer_reqest(req, res, next){    // for broker app api
 		let completed = {};
 		if(users[0].status == '1'){
 			let up_query =
-        "SELECT `users`.`name`, `users`.`email`, `users`.`phone_no`, `users`.`image`, `users`.`address`, `users`.`qualifications`, `book_nows`.`status`, `users`.`about_me`, `book_nows`.`id`, `book_nows`.`location`,`book_nows`.`assign_at` AS `created_at`, `book_nows`.`updated_at` FROM `book_nows` JOIN `users` ON `users`.`id` = `book_nows`.`cus_id` WHERE `book_nows`.`assign_at` >= '" +
+        "SELECT `users`.`name`, `users`.`email`, `users`.`phone_no`, `users`.`image`, `users`.`address`, `users`.`qualifications`, `book_nows`.`status`, `users`.`about_me`, `book_nows`.`id`, `book_nows`.`location`,`book_nows`.`latitude`,`book_nows`.`longitude`,`book_nows`.`assign_at` AS `created_at`, `book_nows`.`updated_at` FROM `book_nows` JOIN `users` ON `users`.`id` = `book_nows`.`cus_id` WHERE `book_nows`.`assign_at` >= '" +
         date_format +
         "' AND (`book_nows`.`status` = 'pending' OR (`book_nows`.`status` = 'in_progress' AND `book_nows`.`broker_id` = '" +
         user.id +
@@ -247,7 +247,7 @@ async function customer_reqest(req, res, next){    // for broker app api
 			upcoming = await qb.query(up_query);
 		}else{
 			let up_query =
-        "SELECT `users`.`name`, `users`.`email`, `users`.`phone_no`, `users`.`image`, `users`.`address`, `users`.`qualifications`, `book_nows`.`status`, `users`.`about_me`, `book_nows`.`id`,  `book_nows`.`location`,`book_nows`.`assign_at` AS `created_at`, `book_nows`.`updated_at` FROM `book_nows` JOIN `users` ON `users`.`id` = `book_nows`.`cus_id` WHERE `book_nows`.`status` = 'in_progress' AND `book_nows`.`assign_at` >= '" +
+        "SELECT `users`.`name`, `users`.`email`, `users`.`phone_no`, `users`.`image`, `users`.`address`, `users`.`qualifications`, `book_nows`.`status`, `users`.`about_me`, `book_nows`.`id`,  `book_nows`.`location`,`book_nows`.`latitude`,`book_nows`.`longitude`,`book_nows`.`assign_at` AS `created_at`, `book_nows`.`updated_at` FROM `book_nows` JOIN `users` ON `users`.`id` = `book_nows`.`cus_id` WHERE `book_nows`.`status` = 'in_progress' AND `book_nows`.`assign_at` >= '" +
         date_format +
         "' AND `book_nows`.`broker_id` = '" +
         user.id +
@@ -265,11 +265,13 @@ async function customer_reqest(req, res, next){    // for broker app api
     }
 		
 		let completed_query =
-      "SELECT `users`.`name`, `users`.`email`, `users`.`phone_no`, `users`.`image`, `users`.`address`, `users`.`qualifications`, `book_nows`.`status`, `users`.`about_me`, `book_nows`.`id`, `book_nows`.`location`, `book_nows`.`assign_at` AS `created_at`, `book_nows`.`updated_at` FROM `book_nows` JOIN `users` ON `users`.`id` = `book_nows`.`cus_id` WHERE `book_nows`.`status` != 'in_progress' AND `book_nows`.`assign_at` <= '" +
+      "SELECT `users`.`name`, `users`.`email`, `users`.`phone_no`, `users`.`image`, `users`.`address`, `users`.`qualifications`, `book_nows`.`status`, `users`.`about_me`, `book_nows`.`id`, `book_nows`.`location`, `book_nows`.`latitude`,`book_nows`.`longitude`,`book_nows`.`assign_at` AS `created_at`, `book_nows`.`updated_at` FROM `book_nows` JOIN `users` ON `users`.`id` = `book_nows`.`cus_id` WHERE `book_nows`.`status` != 'in_progress' AND `book_nows`.`assign_at` <= '" +
       date_format +
       "' AND `book_nows`.`for_broker` LIKE '%" +
       user_id +
-      "%' OR (`book_nows`.`status` = 'cancelled' AND `book_nows`.`broker_id` = "+user.id+") ORDER BY `book_nows`.`id` DESC";
+      "%' OR (`book_nows`.`status` = 'cancelled' AND `book_nows`.`broker_id` = " +
+      user.id +
+      ') ORDER BY `book_nows`.`id` DESC';
 		completed = await qb.query(completed_query);
 		if (req.headers.time_zone) {
       completed = completed.map(function (response) {
